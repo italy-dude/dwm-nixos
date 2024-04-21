82b29cfebf16259b716351b1663e37fc362163ff
italy-dude <italy-dude@users.noreply.github.com>
2024-04-21 12:58:56 +0200

    Update config.h

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const char *brdowncmd[] = { "sudo", "light", "-U", "10", NULL };
 
 static const Key keys[] = {
         /* modifier                     key        function        argument */
-        { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, /* */
+        { MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } }, /* */
         { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_b,      togglebar,      {0} },
         { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
