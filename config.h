6a87cfd02b111a51b9dff0855372b3776e0e08e6
italy-dude <italy-dude@users.noreply.github.com>
2024-04-21 14:33:10 +0200

    Update config.h

--- config.h
+++ config.h
@@ -62,8 +62,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *brupcmd[] = { "light", "-A", "10", NULL };
-static const char *brdowncmd[] = { "light", "-U", "10", NULL };
+static const char *brupcmd[] = { "/run/current-system/sw/bin/light", "-A", "10", NULL };
+static const char *brdowncmd[] = { "/run/current-system/sw/bin/light", "-U", "10", NULL };
 //static const char *brupcmd[] = { "/run/current-system/sw/bin/logger", "bright upppppppp", NULL };
 //static const char *brdowncmd[] = { "/run/current-system/sw/bin/logger", "bright downnnnn", NULL };
 
